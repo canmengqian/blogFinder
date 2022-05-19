9666883bcef967f6ed3574ecc9a5c6f147e9ada9
zhengzuozhang <2570389155@qq.com>
2022-05-19 14:23:36 +0800

    init

---
+++ com.zzz.media.gateway/src/main/java/com/zzz/media/gateway/GateWayApp.java
@@ -0,0 +1,13 @@
+package com.zzz.media.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GateWayApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GateWayApp.class, args);
+    }
+
+}
